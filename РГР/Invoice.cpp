bea729fc07ecf0a49d62ec386234bec945322f5e
pewpewka <lisroma2060@gmail.com>
2025-05-12 16:14:35 +0300

    Додав класи: Customer, Invoice

---
+++ РГР/Invoice.cpp
@@ -0,0 +1,14 @@
+#include "Invoice.h"
+
+Invoice::Invoice(int invoiceNumber, const Customer& customer, const Order& order, const string& dateIssued)
+    : invoiceNumber(invoiceNumber), customer(customer), order(order), dateIssued(dateIssued) {
+}
+
+int Invoice::getInvoiceNumber() const { return invoiceNumber; }
+Customer Invoice::getCustomer() const { return customer; }
+Order Invoice::getOrder() const { return order; }
+string Invoice::getDateIssued() const { return dateIssued; }
+
+double Invoice::calculateTotal() const {
+    return order.getTotalAmount();
+}
--- РГР/Invoice.h
+++ РГР/Invoice.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <string>
+#include "Customer.h"
+#include "Order.h"
+
+using namespace std;
+
+class Invoice {
+private:
+    int invoiceNumber;
+    Customer customer;
+    Order order;
+    string dateIssued;
+
+public:
+    Invoice(int invoiceNumber, const Customer& customer, const Order& order, const string& dateIssued);
+
+    int getInvoiceNumber() const;
+    Customer getCustomer() const;
+    Order getOrder() const;
+    string getDateIssued() const;
+
+    double calculateTotal() const;
+};
