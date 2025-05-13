22725455e91bb16e4a0df622dd270bcf86582455
Nazar Kohut <lisroma2060@gmail.com>
2025-05-13 13:45:27 +0300

    додав класи discount і warehouse

---
+++ РГР/Discount.cpp
@@ -0,0 +1,12 @@
+#include "Discount.h"
+
+Discount::Discount(int id, const string& name, double percentage)
+    : id(id), name(name), percentage(percentage) {
+}
+
+int Discount::getId() const { return id; }
+string Discount::getName() const { return name; }
+double Discount::getPercentage() const { return percentage; }
+
+void Discount::setName(const string& newName) { name = newName; }
+void Discount::setPercentage(double newPercentage) { percentage = newPercentage; }
